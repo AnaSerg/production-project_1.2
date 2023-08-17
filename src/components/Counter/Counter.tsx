a21d7bd941a6af82240a5f08d046533ae0128c60
Anastasia Sergeeva <91882066+AnaSerg@users.noreply.github.com>
2023-08-18 02:17:08 +0300

    Initial Commit

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,8 @@
+import styles from './counter.module.scss';
+export const Counter = () => {
+    return (
+        <div>
+            <button className={styles.btn}>кнопка</button>
+        </div>
+    );
+};
